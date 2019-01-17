0d639b043ac4d57a7c5cf44876455cecb13d9386
王雪松 <wangxuesong0302@autohome.com.cn>
2019-01-17 18:08:07 +0800

    add

---
+++ src/main/java/cn/wangxs/woodpecker/WoodpeckerApplication.java
@@ -0,0 +1,18 @@
+package cn.wangxs.woodpecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangxuesong0302@autohome.com.cn
+ * @version 1.0
+ * <p>
+ * 2018/10/14
+ */
+@SpringBootApplication
+public class WoodpeckerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WoodpeckerApplication.class, args);
+
+    }
+}
